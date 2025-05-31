0534b3586260f07def9d10d90c6855d270263df7
Hektor Misplon <hektor.misplon@pm.me>
2025-05-31 12:24:38 +0200

    Refactor '/etc/users.nix'

--- modules/users.nix
+++ modules/users.nix
@@ -4,9 +4,6 @@
   users.users.h = {
     isNormalUser = true;
     description = "h";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    extraGroups = [ "wheel" "networkmanager" ];
   };
 }
