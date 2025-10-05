7a14323817d164cc02489de70f8d570740050868
Hektor Misplon <hektor.misplon@pm.me>
2025-10-05 16:34:46 +0200

    Add packages from 'dots/.bin/setup'

--- hosts/vm/home.nix
+++ hosts/vm/home.nix
@@ -7,14 +7,64 @@
   home.homeDirectory = "/home/h";
 
   programs = {
-    home-manager.enable = true;
-    git.enable = true;
+    bash = {
+      enable = true;
+      # TODO
+    };
     firefox.enable = true;
+    git.enable = true;
+    home-manager.enable = true;
   };
 
   home.packages = with pkgs; [
+    bash-completion
+    bash-language-server
+    brightnessctl
+    entr
+    eslint_d
+    feh
+    firefox-devedition
+    fzf
+    git
+    haskell-language-server
+    haskellPackages.pandoc-crossref
+    haskellPackages.hadolint
+    htop
+    iosevka
+    jq
+    keepassxc
     kitty
+    lua-language-server
     neovim
+    nmap
+    nodejs_24
+    nodePackages.ts-node
+    nvimpager
+    ormolu
+    pandoc
+    parallel
+    pass
+    pnpm
+    ripgrep
+    sshfs
+    stylelint
+    svelte-language-server
+    tailwindcss-language-server
+    taskwarrior3
+    tldr
+    tmux
+    tmuxp
+    tree
+    tree-sitter
+    typescript-language-server
+    unzip
+    vim-language-server
+    vimPlugins.vim-plug
+    vtsls
+    wget
+    xbanish
+    xclip
+    yaml-language-server
   ];
 
   home.file.".inputrc".source = ../../dots/.inputrc;
