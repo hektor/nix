448e553e8ea3d87679c1d8609f07b9c70c987b47
Hektor Misplon <hektor.misplon@pm.me>
2025-10-28 22:13:22 +0100

    Add 'nixpkgs.follows' to 'nixgl' input in Nix home manager flake

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -1,16 +1,22 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixgl.url = "github:nix-community/nixGL";
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs =
     {
       nixpkgs,
