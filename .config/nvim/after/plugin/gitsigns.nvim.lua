950b09cc5ba5c50c396753f7c0cfd49487142dab
Hektor Misplon <hektor.misplon@pm.me>
2025-11-04 13:22:42 +0100

    Update gitsigns.nvim config

--- .config/nvim/after/plugin/gitsigns.nvim.lua
+++ .config/nvim/after/plugin/gitsigns.nvim.lua
@@ -1,8 +1,12 @@
 local gitsigns = require("gitsigns")
 
 gitsigns.setup({
-  current_line_blame_opts = { delay = 0 },
   current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+  linehl = true,
+  current_line_blame_opts = {
+    delay = 0,
+    virt_text_pos = "right_align",
+  },
 })
 
 vim.api.nvim_create_user_command("Blame", gitsigns.toggle_current_line_blame, { nargs = "?" })
