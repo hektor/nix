5ea1050197e78d6d605ccef3f48429b96b9303d4
Hektor Misplon <hektor.misplon@pm.me>
2025-11-06 12:16:10 +0100

    Fix conform eslint-prettier conflicting formatters

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -1,6 +1,6 @@
 require("conform").setup({
-  format_on_save = {
-    lsp_fallback = true,
+  format_after_save = {
+    lsp_fallback = false,
     async = false,
     timeout_ms = 500,
   },
@@ -16,14 +16,14 @@ require("conform").setup({
     lua = { "stylua" }, -- configured in stylua.toml
     markdown = { "prettierd", "prettier", stop_after_first = true },
     nix = { "nixfmt" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+    javascript = { "eslint_d", "eslint", "prettierd", "prettier", stop_after_first = true },
+    javascriptreact = { "eslint_d", "eslint", "prettierd", "prettier", stop_after_first = true },
     json = { "prettierd", "prettier", stop_after_first = true },
     jsonc = { "prettierd", "prettier", stop_after_first = true },
     python = { "isort", "black" },
-    svelte = { "prettierd", "prettier", stop_after_first = true },
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-    yaml = { "prettierd", "prettier", stop_after_first = true },
+    svelte = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+    typescript = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
+    typescriptreact = { "eslint_d", "eslint", "prettierd", "prettier", stop_after_first = true },
+    -- yaml = { "prettierd", "prettier", stop_after_first = true },
   },
 })
