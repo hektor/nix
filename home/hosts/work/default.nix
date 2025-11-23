5c163d57b055c04c704a2b4e9392077b50e2c6d2
Hektor Misplon <hektor.misplon@pm.me>
2025-11-23 16:10:30 +0100

    Refactor 'home/hosts/work'

--- home/hosts/work/default.nix
+++ home/hosts/work/default.nix
@@ -18,7 +18,7 @@
   sops = {
     defaultSopsFile = "${builtins.toString inputs.nix-secrets}/secrets.yaml";
     defaultSopsFormat = "yaml";
-    age.keyFile = "/home/hektor/.config/sops/age/keys.txt";
+    age.keyFile = "/home/${config.home.username}/.config/sops/age/keys.txt";
 
     secrets."test" = { };
   };
@@ -28,14 +28,16 @@
     defaultWrapper = "mesa";
   };
 
-  programs.anki = import ../../modules/anki.nix;
-  programs.firefox = import ../../modules/firefox.nix {
-    inherit inputs;
-    inherit pkgs;
-    inherit config;
+  programs = {
+    anki = import ../../modules/anki.nix;
+    firefox = import ../../modules/firefox.nix {
+      inherit inputs;
+      inherit pkgs;
+      inherit config;
+    };
+    git = import ../../modules/git.nix;
+    keepassxc = import ../../modules/keepassxc.nix;
   };
-  programs.git = import ../../modules/git.nix;
-  programs.keepassxc = import ../../modules/keepassxc.nix;
   home.packages = import ./packages.nix {
     inherit pkgs;
     inherit config;
