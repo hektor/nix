4e2c3473e420846fb92ea7647de6eccdf020c944
Hektor Misplon <hektor.misplon@pm.me>
2025-12-01 18:23:46 +0100

    update bluetooth config

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -1,3 +1,15 @@
 {
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
 }
