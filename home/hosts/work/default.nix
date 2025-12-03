7369600da73b461d08adb210b352b31a74b2b0a8
Hektor Misplon <hektor.misplon@pm.me>
2025-12-03 14:32:51 +0100

    feat(home): add git config with example configuration for non-NixOS

--- home/hosts/work/default.nix
+++ home/hosts/work/default.nix
@@ -10,20 +10,14 @@ let
 in
 {
   imports = [
-    ../../modules/dconf.nix # TODO: Only enable when on Gnome?
+    ../../modules/dconf.nix
+    ../../modules/git.nix
+    ../../modules/k9s.nix
   ];
 
   home.stateVersion = "25.05";
   home.username = username;
   home.homeDirectory = "/home/${username}";
-
-  sops = {
-    defaultSopsFile = "${builtins.toString inputs.nix-secrets}/secrets.yaml";
-    defaultSopsFormat = "yaml";
-    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-
-    secrets."test" = { };
-  };
 
   nixGL = {
     packages = inputs.nixgl.packages;
@@ -37,7 +31,6 @@ in
       inherit pkgs;
       inherit config;
     };
-    git = import ../../modules/git.nix;
     keepassxc = import ../../modules/keepassxc.nix;
   };
   home.packages = import ./packages.nix {
--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -25,6 +25,8 @@ in
         "taskwarrior_sync_server_url".owner = config.users.users.${cfg.username}.name;
         "taskwarrior_sync_server_client_id".owner = config.users.users.${cfg.username}.name;
         "taskwarrior_sync_encryption_secret".owner = config.users.users.${cfg.username}.name;
+        "email_personal".owner = config.users.users.${cfg.username}.name;
+        "email_work".owner = config.users.users.${cfg.username}.name;
       };
 
       templates."taskrc.d/sync" = {
@@ -33,6 +35,24 @@ in
           sync.server.url=${config.sops.placeholder."taskwarrior_sync_server_url"}
           sync.server.client_id=${config.sops.placeholder."taskwarrior_sync_server_client_id"}
           sync.encryption_secret=${config.sops.placeholder."taskwarrior_sync_encryption_secret"}
+        '';
+      };
+
+      templates.".gitconfig.email" = {
+        owner = config.users.users.${cfg.username}.name;
+        path = "/home/${cfg.username}/.gitconfig.email";
+        content = ''
+          [user]
+            email = ${config.sops.placeholder."email_personal"}
+        '';
+      };
+
+      templates.".gitconfig.work.email" = {
+        owner = config.users.users.${cfg.username}.name;
+        path = "/home/${cfg.username}/.gitconfig.work.email";
+        content = ''
+          [user]
+            email = ${config.sops.placeholder."email_work"}
         '';
       };
     };
