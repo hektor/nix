32a65d912ab6af5f5778005054535e9e29c12a3f
Hektor Misplon <hektor.misplon@pm.me>
2025-12-07 00:04:56 +0100

    chore: ignore breaking changes warning for 'codecompanion.nvim'

--- dots/.config/nvim/after/plugin/codecompanion.nvim.lua
+++ dots/.config/nvim/after/plugin/codecompanion.nvim.lua
@@ -1,13 +1,14 @@
 require("codecompanion").setup({
+  ignore_warnings = true,
   extensions = {
     mcphub = {
       callback = "mcphub.extensions.codecompanion",
       opts = {
         make_vars = true,
         make_slash_commands = true,
-        show_result_in_chat = true
-      }
-    }
+        show_result_in_chat = true,
+      },
+    },
   },
   strategies = {
     chat = { adapter = "openai" },
