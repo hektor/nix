acafbf9a6a709db365e12974431582e291557b14
Hektor Misplon <hektor.misplon@pm.me>
2025-12-15 12:17:17 +0100

    refactor: decouple 'andromache' home config from 'astyanax'

--- home/hosts/andromache/default.nix
+++ home/hosts/andromache/default.nix
@@ -6,18 +6,78 @@
   ...
 }:
 
+let
+  username = "h";
+in
 {
   imports = [
-    (import ../astyanax {
-      inherit inputs;
+    ../../modules/desktop/niri
+    ../../modules/git.nix
+    ../../modules/k9s.nix
+    (import ../../modules/taskwarrior.nix {
       inherit config;
       inherit pkgs;
     })
+    (import ../../modules/keepassxc.nix { inherit pkgs; })
     (import ../../modules/anki.nix {
       inherit config;
       inherit pkgs;
     })
   ];
 
-  programs.taskwarrior.config.recurrence = lib.mkForce "on";
+  home.stateVersion = "25.05";
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
+
+  xdg.userDirs.createDirectories = false;
+  xdg.userDirs.download = "${config.home.homeDirectory}/dl";
+
+  programs = {
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      initExtra = ''
+        for f in /home/h/.bashrc.d/*; do
+          [ -f "$f" ] && source "$f"
+        done
+
+        source /home/h/.bash_aliases/all
+        source /home/h/.bash_aliases/lang-js
+
+        # host-specific config goes here
+        # ...
+
+        export PATH=${../../../dots/.bin}:$PATH
+      '';
+    };
+    firefox = import ../../modules/firefox.nix {
+      inherit inputs;
+      inherit pkgs;
+      inherit config;
+    };
+    fzf = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+    home-manager.enable = true;
+    taskwarrior.config.recurrence = lib.mkForce "on";
+  };
+
+  home.packages = import ../packages.nix {
+    inherit pkgs;
+    inherit config;
+  };
+
+  home.file = {
+    ".inputrc".source = ../../../dots/.inputrc;
+    ".bashrc.d/prompt".source = ../../../dots/.bashrc.d/prompt;
+    ".bashrc.d/editor".source = ../../../dots/.bashrc.d/editor;
+    ".bash_aliases/all".source = ../../../dots/.bash_aliases/all;
+    ".bash_aliases/lang-js".source = ../../../dots/.bash_aliases/lang-js;
+    ".config/kitty/kitty.conf".source = ../../../dots/.config/kitty/kitty.conf;
+    ".config/kitty/themes/zenwritten_light.conf".source =
+      ../../../dots/.config/kitty/themes/zenwritten_light.conf;
+    ".config/kitty/themes/zenwritten_dark.conf".source =
+      ../../../dots/.config/kitty/themes/zenwritten_dark.conf;
+  };
 }
