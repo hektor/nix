07223be2301d082342e68998a02540225dcbc116
hektor <hektor.misplon@pm.me>
2026-01-17 15:09:57 +0100

    fix: update font configuration

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,5 +1,16 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./iosevka.nix
   ];
+
+  fonts = {
+    # disable default font packages (see https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/config/fonts/packages.nix)
+    enableDefaultPackages = false;
+    packages = with pkgs; [
+      dejavu_fonts
+      liberation_ttf
+    ];
+  };
 }
