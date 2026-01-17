f74803cd88262adab1cc37e6ba599cb11c4557cf
hektor <hektor.misplon@pm.me>
2026-01-17 18:02:17 +0100

    fix: remove xdg config

--- modules/desktops/niri/default.nix
+++ modules/desktops/niri/default.nix
@@ -7,10 +7,6 @@
   services.logind.settings.Login = {
     HandleLidSwitch = "suspend";
   };
-  xdg = {
-    portal.enable = true;
-    portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
-  };
 
   services.displayManager.ly = {
     enable = true;
