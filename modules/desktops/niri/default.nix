6a0538c8b8142d7e9f9d64fad0f7598e60f9b02e
hektor <hektor.misplon@pm.me>
2026-01-17 16:33:30 +0100

    fix: improve niri desktop

--- modules/desktops/niri/default.nix
+++ modules/desktops/niri/default.nix
@@ -1,8 +1,19 @@
+{ pkgs, ... }:
+
 {
   programs.niri.enable = true;
 
   services.dbus.enable = true;
+  services.logind.settings.Login = {
+    HandleLidSwitch = "suspend";
+  };
   xdg = {
     portal.enable = true;
+    portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+  };
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
   };
 }
