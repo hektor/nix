5a7afbfe82056edc63cd78b950537aa37fd7eb59
hektor <hektor.misplon@pm.me>
2026-01-17 15:09:57 +0100

    fix: update font configuration

--- modules/fonts/iosevka.nix
+++ modules/fonts/iosevka.nix
@@ -1,7 +1,14 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    (iosevka-bin.override { variant = "SGr-IosevkaTermSS08"; })
-  ];
+  fonts = {
+    packages = with pkgs; [
+      (iosevka-bin.override { variant = "SGr-IosevkaTermSS08"; })
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "Iosevka Term SS08" ];
+      };
+    };
+  };
 }
