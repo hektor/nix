6b1b09f9a666ad232f9a18901e6b86e5398cfa9d
Hektor Misplon <hektor.misplon@pm.me>
2026-01-20 15:01:48 +0100

    refactor: centralize kitty config

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -6,5 +6,14 @@
     programs.bash.shellAliases = {
       icat = "kitty +kitten icat";
     };
+
+    home.file = {
+      ".config/kitty/kitty.conf".source = ../../dots/.config/kitty/kitty.conf;
+      ".config/kitty/nvim.conf".source = ../../dots/.config/kitty/nvim.conf;
+      ".config/kitty/themes/zenwritten_light.conf".source =
+        ../../dots/.config/kitty/themes/zenwritten_light.conf;
+      ".config/kitty/themes/zenwritten_dark.conf".source =
+        ../../dots/.config/kitty/themes/zenwritten_dark.conf;
+    };
   };
 }
