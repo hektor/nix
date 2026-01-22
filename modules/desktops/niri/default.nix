934cbff81d33a9dc399f400567c107e87e2dcfac
hektor <hektor.misplon@pm.me>
2026-01-22 19:55:44 +0100

    feat: configure auto suspend

--- modules/desktops/niri/default.nix
+++ modules/desktops/niri/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
-
 {
   programs.niri.enable = true;
 
   services.dbus.enable = true;
   services.logind.settings.Login = {
     HandleLidSwitch = "suspend";
+    IdleAction = "suspend";
+    IdleActionSec = 1800;
   };
 
   services.displayManager.ly = {
