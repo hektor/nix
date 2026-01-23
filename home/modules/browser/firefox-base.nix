76a31b3051bfc3a1500bc154a6c2a063e5cbbe4b
Hektor Misplon <hektor.misplon@pm.me>
2026-01-23 14:35:06 +0100

    refactor: deduplicate firefox/librewolf config

---
+++ home/modules/browser/firefox-base.nix
@@ -0,0 +1,78 @@
+{ inputs, pkgs }:
+
+let
+  bookmarks = import ./bookmarks.nix;
+in
+
+{
+  nativeMessagingHosts = with pkgs; [
+    tridactyl-native
+  ];
+  profiles = {
+    default = {
+      settings = {
+        "signon.rememberSignons" = false;
+        "findbar.highlightAll" = true;
+        "extensions.autoDisableScopes" = 0;
+      };
+      extensions = {
+        packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+          duckduckgo-privacy-essentials
+          istilldontcareaboutcookies
+          libredirect
+          keepassxc-browser
+          react-devtools
+          sponsorblock
+          tridactyl
+          ublock-origin
+        ];
+      };
+      bookmarks = {
+        force = true;
+        settings = [
+          {
+            toolbar = true;
+            bookmarks = [
+              bookmarks.nixos
+            ];
+          }
+        ];
+      };
+    };
+  };
+  policies = {
+    DefaultDownloadDirectory = "\${home}/dl";
+    ExtensionSettings = {
+      "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+        default_area = "navbar";
+        private_browsing = true;
+      };
+      "idcac-pub@guus.ninja" = {
+        default_area = "navbar";
+        private_browsing = true;
+      };
+      "7esoorv3@alefvanoon.anonaddy.me" = {
+        default_area = "navbar";
+      };
+      "keepassxc-browser@keepassxc.org" = {
+        default_area = "navbar";
+        private_browsing = true;
+      };
+      "@react-devtools" = {
+        default_area = "navbar";
+        private_browsing = true;
+      };
+      "sponsorBlocker@ajay.app" = {
+        default_area = "navbar";
+        private_browsing = true;
+      };
+      "tridactyl.vim@cmcaine.co.uk".settings = {
+        private_browsing = true;
+      };
+      "uBlock0@raymondhill.net".settings = {
+        default_area = "navbar";
+        private_browsing = true;
+      };
+    };
+  };
+}
