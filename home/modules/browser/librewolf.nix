145351a30d9418f4f8ffb24a4d1190b4ff725ba2
Hektor Misplon <hektor.misplon@pm.me>
2026-01-23 14:35:06 +0100

    refactor: deduplicate firefox/librewolf config

--- home/modules/browser/librewolf.nix
+++ home/modules/browser/librewolf.nix
@@ -1,14 +1,4 @@
-{
-  config,
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
-
-let
-  bookmarks = import ./bookmarks.nix;
-in
+{ config, lib, inputs, pkgs, ... }:
 
 {
   config =
@@ -16,76 +6,7 @@ in
       {
         programs.librewolf = {
           enable = true;
-          nativeMessagingHosts = with pkgs; [
-            tridactyl-native
-          ];
-          profiles = {
-            default = {
-              settings = {
-                "signon.rememberSignons" = false;
-                "findbar.highlightAll" = true;
-                "extensions.autoDisableScopes" = 0;
-              };
-              extensions = {
-                packages = with inputs.firefox-addons.packages.${pkgs.system}; [
-                  duckduckgo-privacy-essentials
-                  istilldontcareaboutcookies
-                  libredirect
-                  keepassxc-browser
-                  react-devtools
-                  sponsorblock
-                  tridactyl
-                  ublock-origin
-                ];
-              };
-              bookmarks = {
-                force = true;
-                settings = [
-                  {
-                    toolbar = true;
-                    bookmarks = [
-                      bookmarks.nixos
-                    ];
-                  }
-                ];
-              };
-            };
-          };
-          policies = {
-            DefaultDownloadDirectory = "\${home}/dl";
-            ExtensionSettings = {
-              "jid1-ZAdIEUB7XOzOJw@jetpack" = {
-                default_area = "navbar";
-                private_browsing = true;
-              };
-              "idcac-pub@guus.ninja" = {
-                default_area = "navbar";
-                private_browsing = true;
-              };
-              "7esoorv3@alefvanoon.anonaddy.me" = {
-                default_area = "navbar";
-              };
-              "keepassxc-browser@keepassxc.org" = {
-                default_area = "navbar";
-                private_browsing = true;
-              };
-              "@react-devtools" = {
-                default_area = "navbar";
-                private_browsing = true;
-              };
-              "sponsorBlocker@ajay.app" = {
-                default_area = "navbar";
-                private_browsing = true;
-              };
-              "tridactyl.vim@cmcaine.co.uk".settings = {
-                private_browsing = true;
-              };
-              "uBlock0@raymondhill.net".settings = {
-                default_area = "navbar";
-                private_browsing = true;
-              };
-            };
-          };
-        };
+        }
+        // (import ./firefox-base.nix { inherit inputs pkgs; });
       };
 }
