b5776e547c55a3cfd27ed0e25bb02b6816a976bd
hektor <hektor.misplon@pm.me>
2026-02-07 15:14:42 +0100

    refactor: resolve warnings/errors in checks

--- dots/.config/nvim/lua/skeleton/init.lua
+++ dots/.config/nvim/lua/skeleton/init.lua
@@ -2,10 +2,10 @@ local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("BufNewFile", {
   pattern = "shell.nix",
-  command = "0r ~/.config/nvim/skeletons/shell.nix",
+  command = "0r ~/.config/nvim/skeletons/shell.nix.skeleton",
 })
 
 autocmd("BufNewFile", {
   pattern = "flake.nix",
-  command = "0r ~/.config/nvim/skeletons/flake.nix",
+  command = "0r ~/.config/nvim/skeletons/flake.nix.skeleton",
 })
