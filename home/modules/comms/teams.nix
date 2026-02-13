385fcb3ee11a068e47f993ec602aabc799dc1c09
Hektor Misplon <hektor.misplon@pm.me>
2026-02-13 11:02:42 +0100

    refactor(nixGL): extract 'nixGL' wrapper

--- home/modules/comms/teams.nix
+++ home/modules/comms/teams.nix
@@ -7,8 +7,6 @@
 
 {
   config = lib.mkIf config.comms.teams.enable {
-    home.packages = with pkgs; [
-      (if config.lib ? nixGL then config.lib.nixGL.wrap teams-for-linux else teams-for-linux)
-    ];
+    home.packages = [ (config.nixgl.wrap pkgs.teams-for-linux) ];
   };
 }
