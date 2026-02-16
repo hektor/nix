e87f85710d20b6c087214c24ef5e9f9044ebaf39
Hektor Misplon <hektor.misplon@pm.me>
2026-02-16 20:08:05 +0100

    feat(bluetooth): add 'mpris-proxy' service

--- modules/bluetooth/default.nix
+++ modules/bluetooth/default.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   hardware.bluetooth = {
     enable = true;
@@ -11,5 +13,15 @@
         AutoEnable = true;
       };
     };
+  };
+
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [
+      "network.target"
+      "sound.target"
+    ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
   };
 }
