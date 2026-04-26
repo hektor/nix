efd2771d8c7a30287c279d863f632a801881ceb5
hektor <hektor.misplon@pm.me>
2026-04-26 14:31:43 +0200

    fix: remove tmux hooks

--- home/modules/tmux/default.nix
+++ home/modules/tmux/default.nix
@@ -11,9 +11,5 @@
       enable = true;
       extraConfig = builtins.readFile (dotsPath + "/.config/tmux/tmux.conf");
     };
-
-    home.file = {
-      ".config/tmux/hooks/tmux.ssh.conf".source = dotsPath + "/.config/tmux/hooks/tmux.ssh.conf";
-    };
   };
 }
