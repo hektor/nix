ba90329c81409edb1031f60a7e5a70945213b820
Hektor Misplon <hektor.misplon@pm.me>
2026-05-20 09:27:40 +0200

    fix(browser): resolve firefox 'configPath' home manager error

--- home/modules/browser/firefox.nix
+++ home/modules/browser/firefox.nix
@@ -10,6 +10,7 @@
   config = lib.mkIf (config.browser.primary == "firefox" || config.browser.secondary == "firefox") {
     programs.firefox = {
       enable = true;
+      configPath = "${config.xdg.configHome}/mozilla/firefox";
     }
     // (import ./firefox-base.nix {
       inherit
