461c90cc2556df1d0c4b49fd332e3d6ae88b2b30
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 20:20:54 +0200

    feat(x): add enable option

--- modules/x/default.nix
+++ modules/x/default.nix
@@ -1,13 +1,19 @@
-{
-  services.xserver.windowManager.xmonad = {
-    enable = true;
-    enableContribAndExtras = true;
-    config = builtins.readFile ../../dots/.xmonad/xmonad.hs;
-  };
+{ lib, config, ... }:
 
-  services.xserver = {
-    enable = true;
-    xkb.layout = "us";
-    xkb.variant = "";
+{
+  options.desktop.x.enable = lib.mkEnableOption "xmonad desktop";
+
+  config = lib.mkIf config.desktop.x.enable {
+    services.xserver.windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      config = builtins.readFile ../../dots/.xmonad/xmonad.hs;
+    };
+
+    services.xserver = {
+      enable = true;
+      xkb.layout = "us";
+      xkb.variant = "";
+    };
   };
 }
