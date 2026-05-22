9402c16df08e6fd916df5ecf3716b8ac54543fd8
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 10:21:00 +0200

    feat(mako): add enable option

--- home/modules/mako/default.nix
+++ home/modules/mako/default.nix
@@ -1,5 +1,16 @@
 {
-  services.mako = {
-    enable = true;
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.mako;
+in
+{
+  options.mako.enable = lib.mkEnableOption "mako";
+
+  config = lib.mkIf cfg.enable {
+    services.mako.enable = true;
   };
 }
