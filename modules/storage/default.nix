6f98f066a0c9115e24ebd5957cbda77cc6423cd9
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 20:20:25 +0200

    feat(storage): add enable option

--- modules/storage/default.nix
+++ modules/storage/default.nix
@@ -1,4 +1,10 @@
+{ lib, config, ... }:
+
 {
-  services.udisks2.enable = true;
-  boot.zfs.forceImportRoot = false;
+  options.storage.enable = lib.mkEnableOption "storage";
+
+  config = lib.mkIf config.storage.enable {
+    services.udisks2.enable = true;
+    boot.zfs.forceImportRoot = false;
+  };
 }
