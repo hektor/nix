d152f990f4c1c6b62a85495d1cf9756d0cfc41d7
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 20:20:50 +0200

    feat(users): add enable option

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,10 +1,18 @@
-{ config, ... }:
+{ lib, config, ... }:
 
 {
-  users.users.${config.host.username} = {
-    isNormalUser = true;
-    description = config.host.username;
-    extraGroups = [ "wheel" ];
-    initialPassword = "h";
+  options.my.users.enable = lib.mkEnableOption "user account";
+
+  config = lib.mkIf config.my.users.enable {
+    users.users.${config.host.username} = {
+      isNormalUser = true;
+      description = config.host.username;
+      extraGroups = [
+        "wheel"
+        "render"
+        "video"
+      ];
+      initialPassword = "h";
+    };
   };
 }
