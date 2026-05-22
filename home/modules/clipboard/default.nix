8665a36e3a2efb770b86968177aa9c6b353d7ee4
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 09:49:13 +0200

    feat(clipboard): add enable option

--- home/modules/clipboard/default.nix
+++ home/modules/clipboard/default.nix
@@ -1,7 +1,21 @@
-{ pkgs, ... }:
-
 {
-  home.packages = with pkgs; [
-    wl-clipboard
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.clipboard;
+in
+{
+  options.clipboard = {
+    enable = lib.mkEnableOption "clipboard";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      wl-clipboard
+    ];
+  };
 }
