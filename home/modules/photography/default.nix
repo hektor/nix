3dbd301925d2ef107c6533e3640c0f1ef09bde79
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 09:49:30 +0200

    feat(photography): add enable option

--- home/modules/photography/default.nix
+++ home/modules/photography/default.nix
@@ -1,7 +1,21 @@
-{ pkgs, ... }:
-
 {
-  home.packages = with pkgs; [
-    darktable
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.photography;
+in
+{
+  options.photography = {
+    enable = lib.mkEnableOption "photography";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      darktable
+    ];
+  };
 }
