e0e51558b7ee5e785d9211f223d625b95cb49fdd
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 20:20:46 +0200

    feat(uptime-kuma): add enable option

--- modules/uptime-kuma/default.nix
+++ modules/uptime-kuma/default.nix
@@ -1,30 +1,36 @@
 {
+  lib,
+  config,
   pkgs,
   ...
 }:
 
 {
-  virtualisation.oci-containers = {
-    backend = "docker";
-    containers.uptime-kuma = {
-      image = "louislam/uptime-kuma:latest";
-      ports = [ "127.0.0.1:3001:3001" ];
-      volumes = [ "/var/lib/uptime-kuma:/app/data" ];
-      environment = {
-        TZ = "UTC";
-        UMASK = "0022";
+  options."uptime-kuma".enable = lib.mkEnableOption "uptime-kuma";
+
+  config = lib.mkIf config."uptime-kuma".enable {
+    virtualisation.oci-containers = {
+      backend = "docker";
+      containers.uptime-kuma = {
+        image = "louislam/uptime-kuma:latest";
+        ports = [ "127.0.0.1:3001:3001" ];
+        volumes = [ "/var/lib/uptime-kuma:/app/data" ];
+        environment = {
+          TZ = "UTC";
+          UMASK = "0022";
+        };
+        extraOptions = [
+          "--network=proxiable"
+        ];
       };
-      extraOptions = [
-        "--network=proxiable"
-      ];
     };
-  };
 
-  systemd.tmpfiles.settings."uptime-kuma" = {
-    "/var/lib/uptime-kuma".d = {
-      mode = "0755";
+    systemd.tmpfiles.settings."uptime-kuma" = {
+      "/var/lib/uptime-kuma".d = {
+        mode = "0755";
+      };
     };
-  };
 
-  environment.systemPackages = with pkgs; [ docker-compose ];
+    environment.systemPackages = with pkgs; [ docker-compose ];
+  };
 }
