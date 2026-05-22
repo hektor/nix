a74a18403551499fc61e5f2c7e2dc40628a8b2df
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 09:50:48 +0200

    feat(go): add enable option

--- home/modules/go/default.nix
+++ home/modules/go/default.nix
@@ -1,7 +1,22 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    go
-    gopls
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.go;
+in
+{
+  options.go = {
+    enable = lib.mkEnableOption "Go";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      go
+      gopls
+    ];
+  };
 }
