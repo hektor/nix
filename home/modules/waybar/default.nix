2d4f428fd37c51c518940f89c5a6aca20de6dcd2
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 10:21:54 +0200

    feat(waybar): add enable option

--- home/modules/waybar/default.nix
+++ home/modules/waybar/default.nix
@@ -1,11 +1,24 @@
 {
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.waybar;
+in
+{
   imports = [
     ./settings.nix
     ./style.nix
   ];
 
-  programs.waybar = {
-    enable = true;
-    systemd.enable = true;
+  options.waybar.enable = lib.mkEnableOption "waybar";
+
+  config = lib.mkIf cfg.enable {
+    programs.waybar = {
+      enable = true;
+      systemd.enable = true;
+    };
   };
 }
