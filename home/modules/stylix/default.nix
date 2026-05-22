c3239c544ac7be9fcb1652ac6d807570173ab072
Hektor Misplon <hektor.misplon@pm.me>
2026-05-22 10:24:58 +0200

    feat(stylix): add enable option

--- home/modules/stylix/default.nix
+++ home/modules/stylix/default.nix
@@ -1,30 +1,36 @@
 {
   config,
+  lib,
   inputs,
   pkgs,
   ...
 }:
 
 let
+  cfg = config.my.stylix;
   theme = import ../../../modules/stylix/theme.nix { inherit pkgs; };
 in
 {
   imports = [ inputs.stylix.homeModules.stylix ];
 
-  stylix = {
-    enable = true;
-    inherit (theme)
-      polarity
-      base16Scheme
-      override
-      image
-      ;
-    fonts = {
-      monospace = theme.monospaceFont;
-      serif = config.stylix.fonts.monospace;
-      sansSerif = config.stylix.fonts.monospace;
-      emoji = config.stylix.fonts.monospace;
+  options.my.stylix.enable = lib.mkEnableOption "stylix";
+
+  config = lib.mkIf cfg.enable {
+    stylix = {
+      enable = true;
+      inherit (theme)
+        polarity
+        base16Scheme
+        override
+        image
+        ;
+      fonts = {
+        monospace = theme.monospaceFont;
+        serif = config.stylix.fonts.monospace;
+        sansSerif = config.stylix.fonts.monospace;
+        emoji = config.stylix.fonts.monospace;
+      };
+      targets = import ../../../modules/stylix/targets.nix;
     };
-    targets = import ../../../modules/stylix/targets.nix;
   };
 }
--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -1,36 +1,42 @@
 {
   config,
   inputs,
+  lib,
   pkgs,
   ...
 }:
 
 let
+  cfg = config.my.stylix;
   theme = import ./theme.nix { inherit pkgs; };
 in
 {
   imports = [ inputs.stylix.nixosModules.stylix ];
 
-  stylix = {
-    enable = true;
-    inherit (theme)
-      polarity
-      base16Scheme
-      override
-      image
-      ;
-    fonts = {
-      monospace = theme.monospaceFont;
-      serif = config.stylix.fonts.monospace;
-      sansSerif = config.stylix.fonts.monospace;
-      emoji = config.stylix.fonts.monospace;
-    };
-    autoEnable = true;
-  };
+  options.my.stylix.enable = lib.mkEnableOption "stylix theming";
 
-  home-manager.sharedModules = [
-    {
-      stylix.targets = import ./targets.nix;
-    }
-  ];
+  config = lib.mkIf cfg.enable {
+    stylix = {
+      enable = true;
+      inherit (theme)
+        polarity
+        base16Scheme
+        override
+        image
+        ;
+      fonts = {
+        monospace = theme.monospaceFont;
+        serif = config.stylix.fonts.monospace;
+        sansSerif = config.stylix.fonts.monospace;
+        emoji = config.stylix.fonts.monospace;
+      };
+      autoEnable = true;
+    };
+
+    home-manager.sharedModules = [
+      {
+        stylix.targets = import ./targets.nix;
+      }
+    ];
+  };
 }
