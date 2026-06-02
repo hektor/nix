d90cd78ec2f7d22d666c2dafdcdfbe73745a56e9
hektor <hektor.misplon@pm.me>
2026-06-02 22:33:57 +0200

    feat(andromache): use 'systemd-networkd' networking

--- hosts/andromache/default.nix
+++ hosts/andromache/default.nix
@@ -97,6 +97,8 @@ in
     inputs.colmena.packages.${pkgs.stdenv.hostPlatform.system}.colmena
   ];
 
+  services.resolved.enable = true;
+
   services.locate = {
     enable = true;
     package = pkgs.plocate;
