744e25eb4abf9bea2e22c0812a6b89f27f7918be
Hektor Misplon <hektor.misplon@pm.me>
2026-06-09 15:22:07 +0200

    chore(nvim): disable gitsigns line highlight

--- dots/.config/nvim/after/plugin/gitsigns.nvim.lua
+++ dots/.config/nvim/after/plugin/gitsigns.nvim.lua
@@ -2,7 +2,7 @@ local gitsigns = require("gitsigns")
 
 gitsigns.setup({
   current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
-  linehl = true,
+  -- linehl = true,
   current_line_blame_opts = {
     delay = 0,
     virt_text_pos = "right_align",
